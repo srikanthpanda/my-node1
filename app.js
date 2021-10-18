6eca1335dc85c9a78f1d4dad4b0d8e1a01a1cd27
srikanthpanda <39826532+srikanthpanda@users.noreply.github.com>
2021-10-19 05:25:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials 2 !');
+  res.send('Hello World from IBM Cloud Essentials 3 !');
 })
 
 app.listen(port, host);
